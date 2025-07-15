d8160058cc89d61731bda2382d71dce0861c2e68
Sumit Saha <sumit@learnwithsumit.com>
2025-07-15 19:54:26 +0600

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    staleTimes: {
+      static: 30,
+    },
+  },
+};
 
 export default nextConfig;
